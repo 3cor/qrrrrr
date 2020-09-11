31e24b99a224630c69306596741e16fba70ef0bb
smoogi <spacecorgi@pm.me>
2020-09-11 15:34:52 +0700

    Add local files

---
+++ app.js
@@ -0,0 +1,20 @@
+const fs = require('fs');
+const jpeg = require('jpeg-js');
+const jsQR = require("jsqr");
+
+function readImg(imgPath) {
+    let jpegData = fs.readFileSync(imgPath);
+    let rawImageData = jpeg.decode(jpegData);
+    console.log(rawImageData);
+    return(rawImageData);
+}
+
+function readQR(rawImageData) {
+    const code = jsQR(rawImageData.data, rawImageData.width, rawImageData.height)
+    console.log("Found QR code", code);
+}
+
+modules.export = {
+    readImg,
+    readQR
+}
