2006025ee5bc302a3063e0928d7f6f4d036fe0b1
smoogi <spacecorgi@pm.me>
2020-09-12 17:44:36 +0700

    Add log & add missing 's' to export

--- app.js
+++ app.js
@@ -5,16 +5,17 @@ const jsQR = require("jsqr");
 function readImg(imgPath) {
     let jpegData = fs.readFileSync(imgPath);
     let rawImageData = jpeg.decode(jpegData);
-    console.log(rawImageData);
+    console.log('----READ IMAGE FILE----\n', rawImageData);
     return(rawImageData);
 }
 
 function readQR(rawImageData) {
     const code = jsQR(rawImageData.data, rawImageData.width, rawImageData.height)
-    console.log("Found QR code", code);
+    console.log("----READ QR CODE----\n", code);
 }
 
-module.export = {
+module.exports = {
     readImg,
     readQR
 }
+
