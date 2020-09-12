25fe027916e2f5608cd56260ab2c895b565576b6
smoogi <spacecorgi@pm.me>
2020-09-12 17:58:12 +0700

    Update

--- app.js
+++ app.js
@@ -5,13 +5,12 @@ const jsQR = require("jsqr");
 function readImg(imgPath) {
     let jpegData = fs.readFileSync(imgPath);
     let rawImageData = jpeg.decode(jpegData);
-    console.log('----READ IMAGE FILE----\n', rawImageData);
     return(rawImageData);
 }
 
 function readQR(rawImageData) {
     const code = jsQR(rawImageData.data, rawImageData.width, rawImageData.height)
-    console.log("----READ QR CODE----\n", code);
+    return code
 }
 
 module.exports = {
--- test.js
+++ test.js
@@ -2,5 +2,7 @@ const tsqr = require('./app')
 
 imgFile = process.argv.slice(2)[0]
 
-tsqr.readQR(tsqr.readImg(imgFile))
+const qrData = tsqr.readQR(tsqr.readImg(imgFile))
+console.log('ALL QR DATA:\n', qrData)
+console.log('ONLY data:\n', qrData.data)
 
