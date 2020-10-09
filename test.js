0723645b5b5029251aa797dd2c8d4e1474580fd2
3cor <spacecorgi@pm.me>
2020-10-09 18:15:07 +0700

    Update

--- test.js
+++ test.js
@@ -2,7 +2,6 @@ const tsqr = require('./app')
 
 imgFile = process.argv.slice(2)[0]
 
-const qrData = tsqr.readQR(tsqr.readImg(imgFile))
+c onst qrData = tsqr.readQR(tsqr.readImg(imgFile))
 console.log('ALL QR DATA:\n', qrData)
 console.log('ONLY data:\n', qrData.data)
-
